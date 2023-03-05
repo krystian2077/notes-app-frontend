75920fb1237ddd7eebdf729ce84e76de41789505
Krystian <krystian.potaczek07@gmail.com>
2023-03-05 20:24:49 +0100

    Add fetching notes and add proxy.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import { Note } from "./models/note";
 
-const App = () => (
-    <div className = "App">
-      <header className = "App-header">
-        <img src = {logo}
-             className = "App-logo"
-             alt = "logo"/>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-            className = "App-link"
-            href = "https://reactjs.org"
-            target = "_blank"
-            rel = "noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-);
+export const App = () => {
+  const [notes, setNotes] = useState<Note[]>([]);
 
-export default App;
+  useEffect(() => {
+    async function loadNotes() {
+      try {
+        const response = await fetch("/api/notes", {
+          method: "GET",
+        });
+
+        const notes = await response.json();
+        setNotes(notes);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    loadNotes();
+  }, []);
+
+  return (
+    <>
+      <div className="App">{JSON.stringify(notes)}</div>
+    </>
+  );
+};
