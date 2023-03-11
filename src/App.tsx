3d6bb203fe15b322c50dc3f03f772d4a71e51e8b
Krystian <krystian.potaczek07@gmail.com>
2023-03-11 20:38:27 +0100

    Creating notes from frontend and styling.

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import React, { useEffect, useState } from "react";
 import { Note } from "./components/Note";
 import { Note as NoteModel } from "./models/note";
-import { Col, Container, Row } from "react-bootstrap";
+import { Button, Col, Container, Row } from "react-bootstrap";
 import styles from "./styles/NotesPage.module.css";
+import stylesUtils from "./styles/utils.module.css";
+import * as NotesApi from "./network/notes_api";
+import { AddNoteDialog } from "./components/AddNoteDialog";
 export const App = () => {
   const [notes, setNotes] = useState<NoteModel[]>([]);
+  const [showAddNoteDialog, setShowAddNoteDialog] = useState(false);
 
   useEffect(() => {
     async function loadNotes() {
       try {
-        const response = await fetch("/api/notes", {
-          method: "GET",
-        });
-
-        const notes = await response.json();
+        const notes = await NotesApi.fetchNotes();
         setNotes(notes);
       } catch (error) {
         console.error(error);
@@ -25,6 +25,12 @@ export const App = () => {
   return (
     <>
       <Container>
+        <Button
+          className={`mb-4 ${stylesUtils.blockCenter}`}
+          onClick={() => setShowAddNoteDialog(true)}
+        >
+          Add new note
+        </Button>
         <Row xs={1} md={2} xl={3} className="g-4">
           {notes.map((note) => (
             <Col key={note._id}>
@@ -32,6 +38,15 @@ export const App = () => {
             </Col>
           ))}
         </Row>
+        {showAddNoteDialog && (
+          <AddNoteDialog
+            onDismiss={() => setShowAddNoteDialog(false)}
+            onNoteSaved={(newNote) => {
+              setNotes([...notes, newNote]);
+              setShowAddNoteDialog(false);
+            }}
+          />
+        )}
       </Container>
     </>
   );
